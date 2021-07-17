939c6026f1d19c81603fada181c79afe095ec093
Natnael Gebremichael <natnael92460@gmail.com>
2021-07-17 13:37:38 +1200

    New Project
    Simple Exercise

---
+++ C-Sharp-Udemy-Exercise/C-Sharp-Udemy-Exercise/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace C_Sharp_Udemy_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.WriteLine("Hi Ruhann Hello World!");
+            string readLine = Console.ReadLine();
+            Console.Clear(); //
+            Console.WriteLine("Hello World! {0}", readLine);
+            Console.Beep();
+        }
+        /// <summary>
+        /// this is cool Method haha :)
+        /// </summary>
+        static void coolMethod()
+        {
+
+        }
+    }
+}
